36cbd4a6353137f55ea94e54d04c693ff5fba5d4
Neal Grantham <neal@nsgrantham.com>
2021-05-23 18:45:45 -0700

    Write intermediate JSON file, add new fields

--- postprocess.js
+++ postprocess.js
@@ -1,7 +1,9 @@
-import { readJSON, writeCSV, removeFile } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
+import { readJSON, writeJSON, writeCSV, removeFile } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
 
 const filename = Deno.args[0]
 const json = await readJSON(filename)
+
+await writeJSON(filename, json)
 
 const roundHundredth = x => +parseFloat(x).toFixed(2)
 
@@ -21,22 +23,31 @@ const replaceMonth = date => (
     .replace("Dec", "12")
 )
 
+// https://pdssbn.astro.umd.edu/data_other/objclass.shtml
+
 const makeAsteroidRecord = asteroid => (
   {
-    asteroid_name: asteroid.name,
-    close_approach_datetime: replaceMonth(asteroid.close_approach_data[0].close_approach_date_full),
+    id: asteroid.id,
+    name: asteroid.name,
     min_diameter_m: roundHundredth(asteroid.estimated_diameter.meters.estimated_diameter_min),
     max_diameter_m: roundHundredth(asteroid.estimated_diameter.meters.estimated_diameter_max),
+    absolute_magnitude_h: asteroid.absolute_magnitude_h,
+    close_approach_datetime: replaceMonth(asteroid.close_approach_data[0].close_approach_date_full),
     relative_velocity_kmps: roundHundredth(asteroid.close_approach_data[0].relative_velocity.kilometers_per_second),
     miss_distance_lunar: roundHundredth(asteroid.close_approach_data[0].miss_distance.lunar),
     orbiting_body: asteroid.close_approach_data[0].orbiting_body,
-    orbit_uncertainty: asteroid.orbital_data.orbit_uncertainty,
-    orbit_determination_datetime: asteroid.orbital_data.orbit_determination_date,
     first_observation_date: asteroid.orbital_data.first_observation_date,
     last_observation_date: asteroid.orbital_data.last_observation_date,
-    observations_used: asteroid.orbital_data.observations_used,  
+    orbit_id: asteroid.orbital_data.orbit_id,
+    orbit_determination_datetime: asteroid.orbital_data.orbit_determination_date,
+    orbit_uncertainty: asteroid.orbital_data.orbit_uncertainty,
+    orbit_observations_used: asteroid.orbital_data.observations_used,
+    orbital_period_days: asteroid.orbital_data.orbital_period, 
+    orbit_class: asteroid.orbital_data.orbit_class.orbit_class_type,
+    min_orbit_intersection: asteroid.orbital_data.minimum_orbit_intersection,
     is_potentially_hazardous: asteroid.is_potentially_hazardous_asteroid,
     is_sentry_object: asteroid.is_sentry_object,
+    nasa_jpl_url: asteroid.nasa_jpl_url,
   }
 )
 
